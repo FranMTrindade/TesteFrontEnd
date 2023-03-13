75abaedf46b4ca9d61339b9bb4866d8ac5d987e6
Francisco de Assis <contato@franciscoprog.com>
2023-03-13 18:55:16 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard/index';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        {isLoggedIn ? (
+          <Route path="/dashboard" element={<Dashboard />} />
+        ) : null}
+      </Routes>
+      <ToastContainer autoClose={3000} />
+    </>
   );
 }
 
